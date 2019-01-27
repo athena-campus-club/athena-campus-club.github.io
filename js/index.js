5fe4c79fc3305ac1ca98ee7309efb65ed8a187b2
Smruti Ranjan Rana <smrutiranjan.offc@gmail.com>
2019-01-27 20:18:12 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $(window).on('load', function () {
         console.log($(window).width()) //588
-        // setInterval(function () {
+         setInterval(function () {
 
             if ($(window).width() <= 550) {
                 $('.member-cards .card').removeClass('horizontal');
@@ -9,7 +9,7 @@ $(document).ready(function () {
             } else {
                 $('.member-cards .card').addClass('horizontal');
             }
-        // }, 10)
+         }, 10)
     });
 
 
