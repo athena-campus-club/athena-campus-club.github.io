0642d8585e85b65c24ba62ececeeddeecd0d9d18
Smruti Ranjan Rana <smrutiranjan.offc@gmail.com>
2019-01-27 20:12:24 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $(window).on('load', function () {
+        console.log($(window).width()) //588
+        // setInterval(function () {
+
+            if ($(window).width() <= 550) {
+                $('.member-cards .card').removeClass('horizontal');
+
+            } else {
+                $('.member-cards .card').addClass('horizontal');
+            }
+        // }, 10)
+    });
+
+
+});
